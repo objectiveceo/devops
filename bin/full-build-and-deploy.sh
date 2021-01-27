4c9d4ff9eb5d142da074d7c5b6f871e9619da4bd
Grayson <grayson.hansard@gmail.com>
2021-01-26 20:18:44 -0500

    Add single file to build and deploy in one step

---
+++ bin/full-build-and-deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+readonly BIN="$(cd "$(dirname "${BASH_SOURCE[@]}")"; pwd)"
+readonly SSH_IMAGE_DESTINATION="${SSH_IMAGE_DESTINATION:?Provide SSH image destination}"
+
+function main {
+	local logfile=${LOG_FILE:-"__build.log"}
+	
+	"${BIN}/build-image.sh" | tee "$logfile"
+	local imagePath="$(tail -n1 "$logfile")"
+	
+	"${BIN}/push-image.sh" "$imagePath" "$SSH_IMAGE_DESTINATION" | tee -a "$logfile"
+	local remoteImage="$(tail -n1 "$logfile")"
+
+	local imageName="$(grep Building "$logfile" | sed 's/Building //')"
+	"${BIN}/install-remote-image.sh" "$remoteImage" "$imageName" | tee -a "$logfile"
+}
+
+main "$@"
