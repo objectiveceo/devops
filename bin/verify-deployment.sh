c69da4dc259e28ffee8c2189db6867c676b4e21d
Grayson <grayson.hansard@gmail.com>
2021-01-27 20:35:36 -0500

    Add statement about how verification is run

--- bin/verify-deployment.sh
+++ bin/verify-deployment.sh
@@ -3,6 +3,12 @@
 set -euo pipefail
 
 function main {
+	if [[ ! -z $VERIFY_FILE ]]; then
+		echo "Running verification using $VERIFY_FILE"
+	else
+		echo "Running verification using stdin"
+	fi
+
 	while read -r line; do
 		printf "$line"
 		local output="$(curl --max-time 1 -IL "$line" 2> /dev/null)"
