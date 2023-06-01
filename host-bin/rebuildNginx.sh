ea5dfbeb1f6f2ec5113321e61c5295f6b70148a7
Grayson Hansard <grayson.hansard@gmail.com>
2023-05-31 21:07:59 -0400

    Update rebuildNginx.sh for v2

--- host-bin/rebuildNginx.sh
+++ host-bin/rebuildNginx.sh
@@ -5,10 +5,11 @@ docker create \
 	--name ngx \
 	--publish 80:80 \
 	--publish 443:443 \
-	--volume "${HOME}/objectiveceo/":"/objectiveceo/" \
+	--volume "${HOME}/objectiveceo/v2":"/objectiveceo/" \
+	--volume "${HOME}/objectiveceo/ssl":"/objectiveceo/ssl" \
 	--volume "/etc/letsencrypt/":"/etc/letsencrypt/" \
 	--add-host=host.docker.internal:host-gateway \
 	nginx:alpine
-docker cp ~/objectiveceo/objectiveceo.conf ngx:/etc/nginx/conf.d/
-docker cp ~/app/bethany.conf ngx:/etc/nginx/conf.d/
+# docker cp ~/objectiveceo/objectiveceo.conf ngx:/etc/nginx/conf.d/
+docker cp ~/objectiveceo/v2.objectiveceo.conf ngx:/etc/nginx/conf.d/
 docker start ngx
